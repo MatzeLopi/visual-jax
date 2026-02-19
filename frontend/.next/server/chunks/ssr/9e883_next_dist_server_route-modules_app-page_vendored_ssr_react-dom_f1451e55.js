3493a174365faa0c72d213d1303e8480580a31b5
Matthias Lopinski <70134088+MatzeLopi@users.noreply.github.com>
2026-02-19 20:32:46 +0100

    Untracked .next

--- frontend/.next/server/chunks/ssr/9e883_next_dist_server_route-modules_app-page_vendored_ssr_react-dom_f1451e55.js
+++
@@ -1,3 +0,0 @@
-module.exports=[97134,(a,b,c)=>{"use strict";b.exports=a.r(31699).vendored["react-ssr"].ReactDOM}];
-
-//# sourceMappingURL=9e883_next_dist_server_route-modules_app-page_vendored_ssr_react-dom_f1451e55.js.map
--- frontend/.next/server/chunks/ssr/frontend__next-internal_server_app_editor_page_actions_d565dec6.js
+++
@@ -1,3 +0,0 @@
-module.exports=[15557,(a,b,c)=>{}];
-
-//# sourceMappingURL=frontend__next-internal_server_app_editor_page_actions_d565dec6.js.map
